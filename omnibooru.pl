74b20ea6ea5661f74e4e81fe92e402928fe75991
bucketT <devnull@bucketT>
2012-02-20 18:19:25 +0100

    Oh crap, shine my dear

--- omnibooru.pl
+++ omnibooru.pl
@@ -11,9 +11,9 @@ require "common/common.pm";
 
 
 sub main {
-	my ($filter, $software, $tags, $verbose);
+	my ($filter, $software, $tags, $verbose, $omnibooru);
 	
-	Omnibooru::Common->printHeader();
+	Omnibooru::Common->printHeader;
 
 	GetOptions('e' => sub { $filter = 'e'},
 		's' => sub { $filter = 's'},
@@ -21,11 +21,10 @@ sub main {
 	
 	if($#ARGV != 1) { Omnibooru::Common->printHelp; die("\n"); }
 	($software, $tags) = @ARGV;
-	my $booru;
 
-	if($software eq "gelbooru" || $software eq "1") { require "gelbooru/gelbooru.pm"; $software = 'Gelbooru' }
-	#if($software eq "danbooru" || $software eq "2") { require "danbooru/danbooru.pm"; }
+	if($software eq "gelbooru" || $software eq "1") { require "gelbooru/gelbooru.pm"; $omnibooru = 'Gelbooru' }
+	#if($software eq "danbooru" || $software eq "2") { require "danbooru/danbooru.pm"; $omnibooru = 'Danbooru' }
 
-	my $omnibooru = $software->new('tags' => $tags, 'filter' => $filter, 'verbose' => $verbose);
+	$omnibooru = $omnibooru->new('tags' => $tags, 'filter' => $filter, 'verbose' => $verbose);
 	$omnibooru->process();
 }
