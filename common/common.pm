bcf74dd3814cf8b2297a32d81b5f4f6498fc0ef0
bucketT <devnull@bucketT>
2012-02-20 18:00:59 +0100

    Getting used to Perl OOP

--- common/common.pm
+++ common/common.pm
@@ -1,92 +1,110 @@
-sub askForDownload {
-	my($count, $tags) = @_;
-	
-	printf("[+] %i '%s' related images found\n", $count, $tags);
-	printf("[?] Start download? y/n: ");
-	chomp(my $answer = <STDIN>);
-	if ($answer eq "y") {
-		printf("\n")
-	} elsif ($answer eq "n") {
-		die "[-] Aborted\n";
-	} else {
-		die "[-] y/n, stupid!\n";
-	}
-}
+{
+package Omnibooru::Common;
 
-sub processFile {
-	my ($fileURL, $rating, $md5, $filter, $folder, $verbose, $statsRef) = @_;
+
+	sub askForDownload {
+		my $self = shift;
+		my %params = @_;
+		
+		if ($params{'count'} == 0) { die("[-] Empty result\n"); };
 	
-	use Digest::MD5;
-	use LWP::UserAgent;
-	
-	$fileURL =~ m/.*\/(.*\.(jpg|jpeg|png|gif))/;
-	my $fileName = $1;
-	
-	if (-e $folder . '/' . $fileName) {
-		my $file = $folder . '/' . $fileName;
-		open(FILE, $file) or die "[-] Can't open '$file': $!\n";
-		binmode(FILE);
-		if (Digest::MD5->new->addfile(*FILE)->hexdigest eq $md5) {
-			$statsRef->{c}++;
-			beVerbose($verbose, "[+] File ($fileName) exists and MD5 matches\n");
-			return;
-		}
-		else {
-			$statsRef->{h}++;
-			beVerbose($verbose, "[-] File ($fileName) exists but MD5 doesn't match\n");
-		}
-		close(FILE);
-	}
-	
-	if ($filter) {
-		if ($rating ne $filter) {
-			$statsRef->{f}++;
-			beVerbose($verbose, "[-] File ($fileName) doesn't match filter settings\n");
-			return;
+		printf("[+] %i '%s' related images found\n", $params{'count'}, $self->{'tags'});
+		printf("[?] Start download? y/n: ");
+		chomp(my $answer = <STDIN>);
+		if ($answer eq "y") {
+			printf("\n")
+		} elsif ($answer eq "n") {
+			die "[-] Aborted\n";
+		} else {
+			die "[-] y/n, stupid!\n";
 		}
 	}
-	
-	my $ua = LWP::UserAgent->new;
-	beVerbose($verbose, "[+] Downloading '$fileName'\n");
-	$ua->get($fileURL, ":content_file" => $folder . '/' . $fileName);
-	$statsRef->{d}++;
-}
 
-sub beVerbose {
-	my ($verbose, $string) = @_;
+	sub processFile {
+		my $self = shift;
+		my %params = @_;
 	
-	if ($verbose) {
-		print $string;
+		my $filter = $self->{'filter'};
+		my $verbose = $self->{'verbose'};
+	
+	
+		use Digest::MD5;
+		use LWP::UserAgent;
+	
+		$params{'fileURL'} =~ m/.*\/(.*\.(jpg|jpeg|png|gif))/;
+		my $fileName = $1;
+	
+		if (-e $params{'folder'} . '/' . $fileName) {
+			my $file = $params{'folder'} . '/' . $fileName;
+			open(FILE, $file) or die "[-] Can't open '$file': $!\n";
+			binmode(FILE);
+			if (Digest::MD5->new->addfile(*FILE)->hexdigest eq $params{'md5'}) {
+				$params{'statsRef'}->{c}++;
+				$self->beVerbose('string' => "[+] File ($fileName) exists and MD5 matches\n");
+				return;
+			}
+			else {
+				$params{'statsRef'}->{h}++;
+				$self->beVerbose('string' => "[-] File ($fileName) exists but MD5 doesn't match\n");
+			}
+			close(FILE);
+		}
+	
+		if ($filter) {
+			if ($params{'rating'} ne $filter) {
+				$params{'statsRef'}->{f}++;
+				$self->beVerbose('string' => "[-] File ($fileName) doesn't match filter settings\n");
+				return;
+			}
+		}
+	
+		my $ua = LWP::UserAgent->new;
+		$self->beVerbose('string' => "[+] Downloading '$fileName'\n");
+		$ua->get($params{'fileURL'}, ":content_file" => $params{'folder'} . '/' . $fileName);
+		$params{'statsRef'}->{d}++;
+	}
+
+	sub beVerbose {
+		my $self = shift;
+		my %params = @_;
+	
+		if ($self->{'verbose'}) {
+			print $params{'string'};
+		}
+	}
+
+	sub printStats {
+		my $self = shift;
+		my %params = @_;
+
+		printf("\n[+] Success:\n");
+		printf(" --> D: %i\n", $params{'statsRef'}->{d});
+		printf(" --> E: %i\n", $params{'statsRef'}->{c});
+		printf(" --> H: %i\n", $params{'statsRef'}->{h});
+		printf(" --> S: %i\n", $params{'statsRef'}->{f});
+	}
+
+	sub printHeader {
+		my $self = shift;
+	
+		printf("\n.:: omnibooru.pl - MrLoom ::.\n\n");
+	}
+
+	sub printHelp {
+		my $self = shift;
+	
+		printf("Usage: omnibooru.pl [option] software tags\n\n");
+		printf("Options:\n");
+		printf(" -s -> Safe content only\n");
+		printf(" -e -> Explicit content only\n");
+		printf(" -v -> Verbose\n\n");
+		printf("Software:\n");
+		printf(" 1 or gelbooru for Gelbooru\n");
+		printf(" 2 or danbooru for Danbooru (NOT YET)\n\n");
+		printf("Tags:\n");
+		printf(" eg. kinoshita_hideyoshi\n\n");
+		printf("Example:\n");
+		printf(" ./omnibooru -v -e 1 kinoshita_hideyoshi\n");
 	}
 }
-
-sub printStats {
-	my ($statsRef) = @_;
-	
-	printf("\n[+] Success:\n");
-	printf(" --> D: %i\n", $statsRef->{d});
-	printf(" --> E: %i\n", $statsRef->{c});
-	printf(" --> H: %i\n", $statsRef->{h});
-	printf(" --> S: %i\n", $statsRef->{f});
-}
-
-sub printHeader {
-	printf("\n.:: omnibooru.pl - MrLoom ::.\n\n");
-}
-
-sub printHelp {
-	printf("Usage: omnibooru.pl [option] software tags\n\n");
-	printf("Options:\n");
-	printf(" -s -> Safe content only\n");
-	printf(" -e -> Explicit content only\n");
-	printf(" -v -> Verbose\n\n");
-	printf("Software:\n");
-	printf(" 1 or gelbooru for Gelbooru\n");
-	printf(" 2 or danbooru for Danbooru (NOT YET)\n\n");
-	printf("Tags:\n");
-	printf(" eg. kinoshita_hideyoshi\n\n");
-	printf("Example:\n");
-	printf(" ./omnibooru -v -e 1 kinoshita_hideyoshi\n");
-}
-
 1;
--- omnibooru.pl
+++ omnibooru.pl
@@ -13,19 +13,19 @@ require "common/common.pm";
 sub main {
 	my ($filter, $software, $tags, $verbose);
 	
-	&printHeader();
+	Omnibooru::Common->printHeader();
 
-	GetOptions	('e' => sub { $filter = 'e'},
-				 's' => sub { $filter = 's'},
-				 'v' => sub { $verbose = 1});
+	GetOptions('e' => sub { $filter = 'e'},
+		's' => sub { $filter = 's'},
+		'v' => sub { $verbose = 1});
 	
-	if($#ARGV != 1) { &printHelp; die("\n"); }
-	$software = shift(@ARGV);
-	$tags = shift(@ARGV);
+	if($#ARGV != 1) { Omnibooru::Common->printHelp; die("\n"); }
+	($software, $tags) = @ARGV;
+	my $booru;
 
-	if($software eq "gelbooru" || $software eq "1") { require "gelbooru/gelbooru.pm"; }
+	if($software eq "gelbooru" || $software eq "1") { require "gelbooru/gelbooru.pm"; $software = 'Gelbooru' }
 	#if($software eq "danbooru" || $software eq "2") { require "danbooru/danbooru.pm"; }
 
-	&initOmnibooru($tags, $filter, $verbose);
-	
+	my $omnibooru = $software->new('tags' => $tags, 'filter' => $filter, 'verbose' => $verbose);
+	$omnibooru->process();
 }
